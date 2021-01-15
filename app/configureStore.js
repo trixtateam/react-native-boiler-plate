a76db2c76fc55cf1d7f219d1647f4d621ee56a41
Jacques <jacques@trixta.com>
2021-01-15 12:37:02 +0200

    update: linting

--- app/configureStore.js
+++ app/configureStore.js
@@ -2,7 +2,7 @@
  * Create the store with dynamic reducers
  */
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { createInjectorsEnhancer } from 'redux-injectors'
+import { createInjectorsEnhancer } from 'redux-injectors';
 import { createPhoenixChannelMiddleware } from '@trixta/phoenix-to-redux';
 import Reactotron from './ReactotronConfig';
 import createSagaMiddleware from 'redux-saga';
